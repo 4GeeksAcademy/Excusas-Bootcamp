12f249e8dba6346f83435c24fd135b779607430f
Alvaro Ponce Marin <147168257+superteclas@users.noreply.github.com>
2024-02-12 12:20:47 +0000

    mejor

--- src/app.js
+++ src/app.js
@@ -6,7 +6,8 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //  función generadorexcusas//
+  //función generadorexcusas//
+
   function generadorexcusas() {
     // arrays para cada parte de la excusa//
     let quien = ["Un marciano", "Mi abuela", "Mi bebé", "Mi pez"];
@@ -20,13 +21,13 @@ window.onload = function() {
       "mientras estaba rezando"
     ];
 
-    //  Aleatorio para cada parte de la excusa//
+    //Aleatorio para cada parte de la excusa//
     let randomquien = Math.floor(Math.random() * quien.length);
     let randomaccion = Math.floor(Math.random() * accion.length);
     let randomque = Math.floor(Math.random() * que.length);
     let randomcuando = Math.floor(Math.random() * cuando.length);
 
-    // Variable con las partes//
+    //Variable con las partes//
     let frase =
       quien[randomquien] +
       " " +
@@ -37,11 +38,11 @@ window.onload = function() {
       cuando[randomcuando] +
       ".";
 
-    // Muestra la excusa en el HTML
-
+    // Sacar la excusa al HTML (preguntar en clase que no me aclaro del todo)//
+    //console.log(frase);//
     document.getElementById("excusa").innerHTML = frase;
   }
 
-  // Llama a la función para generar una excusa
+  //Llama a la función para generar una excusa//
   generadorexcusas();
 };
