7cc65d73bbabcd01915d8988f69f2e1b55ca6ede
Alvaro Ponce Marin <147168257+superteclas@users.noreply.github.com>
2024-02-12 12:07:49 +0000

    funciona generador de excusas

--- src/app.js
+++ src/app.js
@@ -6,6 +6,41 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //  función generadorexcusas
+  function generadorexcusas() {
+    // arrays para cada parte de la excusa
+    let quien = ["Un marciano", "Mi abuela", "Mi bebé", "My pez"];
+    let accion = ["rompió", "quemó", "mojó", "cocinó"];
+    let que = ["mi proyecto", "las llaves", "el coche"];
+    let cuando = [
+      "después de clase",
+      "antes de clase",
+      "después de comer",
+      "durante el almuerzo",
+      "mientras estaba rezando"
+    ];
+
+    // Genera índices aleatorios para cada parte de la excusa
+    let randomquien = Math.floor(Math.random() * quien.length);
+    let randomaccion = Math.floor(Math.random() * accion.length);
+    let randomque = Math.floor(Math.random() * que.length);
+    let randomcuando = Math.floor(Math.random() * cuando.length);
+
+    // Construye la excusa usando las partes elegidas al azar
+    let frase =
+      quien[randomquien] +
+      " " +
+      accion[randomaccion] +
+      " " +
+      que[randomque] +
+      " " +
+      cuando[randomcuando] +
+      ".";
+
+    // Muestra la excusa en el HTML
+    document.getElementById("excusa").innerHTML = frase;
+  }
+
+  // Llama a la función para generar una excusa
+  generadorexcusas();
 };
